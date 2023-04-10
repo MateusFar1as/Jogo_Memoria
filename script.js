10a5fe3de4633af3f387ba1ae9f1f261d64247d3
Mateus Farias <mateused51@hotmail.com>
2023-04-10 12:36:04 -0300

    mudanca cartoes

--- script.js
+++ script.js
@@ -1,34 +1,47 @@
 const cartas = document.querySelectorAll('.card')
+const cartasImg = document.querySelectorAll('.imgCards')
 let carta1 = 0
 let carta2 = 0
 let x = 0
 let carta1num = 0
 let carta2num = 0
 let numsSalvos = []
+let ItemCartas = ['/imgs/Endymion.png','/imgs/Endymion.png','/imgs/Slifer.jpg','/imgs/Slifer.jpg','/imgs/Endymion.png','/imgs/Endymion.png','/imgs/Slifer.jpg','/imgs/Slifer.jpg']
 
 function teste(num) {
-    if(x == 0 && !numsSalvos.includes(num)) {
-        carta1 = cartas[num]
-        carta1num = num
-        carta1.classList.add('animation')
-        x++
-    } else if(x != 0 && carta1 !== cartas[num] && !numsSalvos.includes(num)) {
-        carta2 = cartas[num]
-        carta2num = num
-        carta2.classList.add('animation')
+    if(!numsSalvos.includes(num) && (!carta2 || !carta2.classList.contains('animation')) ){
+        if(x == 0) {
+            carta1 = cartas[num]
+            carta1num = num
+            carta1.classList.add('animation')
+            x++
+        } else if(x != 0 && carta1 !== cartas[num]) {
+            carta2 = cartas[num]
+            carta2num = num
+            carta2.classList.add('animation')
 
-        if(carta1.innerHTML === carta2.innerHTML) {
-            console.log('correto')
-            carta1 = 0
-            carta2 = 0
-            numsSalvos.push(carta1num,carta2num)
-        }else {
-            carta1.classList.remove('animation')
-            carta2.classList.remove('animation')
+            if(carta1.innerHTML === carta2.innerHTML) {
+                console.log('correto')
+                carta1 = 0
+                carta2 = 0
+                numsSalvos.push(carta1num,carta2num)
+            }else {
+                setTimeout(() => {
+                    carta1.classList.remove("animation");
+                    carta2.classList.remove("animation");
+                }, 1400);
+            }
+            x = 0 
         }
-
-        x = 0 
     }
-
-    console.log(numsSalvos)
 }
+
+function randomizar() {
+    ItemCartas.sort(() => Math.random() - 0.5)
+    cartasImg.forEach((e,i) => {
+        e.setAttribute('src', ItemCartas[i])
+    })
+    numsSalvos.length = 0
+}
+
+randomizar()
